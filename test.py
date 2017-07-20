62240515426486f1d2c0c22c5c99a348c6da448a
Joshua Tsang <joshua.mh.tsang@gmail.com>
2017-07-20 18:35:48 +0100

    changed test.py to have more general starting script for orientxyz orientations

--- test.py
+++ test.py
@@ -6,50 +6,58 @@ origin = np.array([0.0,0.0,0.0])
 
 
 ## lattice fcc ${latticeConstant} orient x 1 0 0 orient y 0 1 0 orient z 0 0 1
-#Ncellx = 2
-#Ncelly = 2
-#Ncellz = 2
-#lattice_constant = 1.0
-#xdir_celllength = lattice_constant
-#ydir_celllength = lattice_constant
-#zdir_celllength = lattice_constant
-#xdir_supercelllength = xdir_celllength*Ncellx
-#ydir_supercelllength = ydir_celllength*Ncelly
-#zdir_supercelllength = zdir_celllength*Ncellz
-#orientx = np.array([1.0, 0.0, 0.0])
-#orienty = np.array([0.0, 1.0, 0.0])
-#orientz = np.array([0.0, 0.0, 1.0])
+# Ncellx = 3
+# Ncelly = 3
+# Ncellz = 3
+# lattice_constant = 1.0
+# orientx = np.array([1.0, 0.0, 0.0])
+# orienty = np.array([0.0, 1.0, 0.0])
+# orientz = np.array([0.0, 0.0, 1.0])
+# pythagorasFactorx = np.linalg.norm(orientx)
+# pythagorasFactory = np.linalg.norm(orienty)
+# pythagorasFactorz = np.linalg.norm(orientz)
+# xdir_celllength = lattice_constant*pythagorasFactorx
+# ydir_celllength = lattice_constant*pythagorasFactory
+# zdir_celllength = lattice_constant*pythagorasFactorz
+# xdir_supercelllength = xdir_celllength*Ncellx
+# ydir_supercelllength = ydir_celllength*Ncelly
+# zdir_supercelllength = zdir_celllength*Ncellz
 
 ## lattice fcc ${latticeConstant} orient x 1 0 0 orient y 0 1 1 orient z 0 -1 1
-#Ncellx = 4
-#Ncelly = 4
-#Ncellz = 4
-#lattice_constant = 1.0
-#xdir_celllength = lattice_constant
-#ydir_celllength = np.sqrt(2)*lattice_constant
-#zdir_celllength = np.sqrt(2)*lattice_constant
-#xdir_supercelllength = xdir_celllength*Ncellx
-#ydir_supercelllength = ydir_celllength*Ncelly
-#zdir_supercelllength = zdir_celllength*Ncellz
-#orientx = np.array([ 1.0,  0.0, 0.0])
-#orienty = np.array([ 0.0,  1.0, 1.0])
-#orientz = np.array([ 0.0, -1.0, 1.0])
+# Ncellx = 3
+# Ncelly = 3
+# Ncellz = 3
+# lattice_constant = 1.0
+# orientx = np.array([ 1.0,  0.0, 0.0])
+# orienty = np.array([ 0.0,  1.0, 1.0])
+# orientz = np.array([ 0.0, -1.0, 1.0])
+# pythagorasFactorx = np.linalg.norm(orientx)
+# pythagorasFactory = np.linalg.norm(orienty)
+# pythagorasFactorz = np.linalg.norm(orientz)
+# xdir_celllength = lattice_constant*pythagorasFactorx
+# ydir_celllength = lattice_constant*pythagorasFactory
+# zdir_celllength = lattice_constant*pythagorasFactorz
+# xdir_supercelllength = xdir_celllength*Ncellx
+# ydir_supercelllength = ydir_celllength*Ncelly
+# zdir_supercelllength = zdir_celllength*Ncellz
 
 ## lattice fcc ${latticeConstant} orient x -1 -1 2 orient y 1 -1 0 orient z 1 1 1
-#Ncellx = 1
-#Ncelly = 1
-#Ncellz = 1
-#lattice_constant = 1.0
-#xdir_celllength = np.sqrt(6)*lattice_constant
-#ydir_celllength = np.sqrt(2)*lattice_constant
-#zdir_celllength = np.sqrt(3)*lattice_constant
-#xdir_supercelllength = xdir_celllength*Ncellx
-#ydir_supercelllength = ydir_celllength*Ncelly
-#zdir_supercelllength = zdir_celllength*Ncellz
-#orientx = np.array([-1.0, -1.0, 2.0])
-#orienty = np.array([ 1.0, -1.0, 0.0])
-#orientz = np.array([ 1.0,  1.0, 1.0])
-
+# Ncellx = 3
+# Ncelly = 3
+# Ncellz = 3
+# lattice_constant = 1.0
+# orientx = np.array([ -1.0,  -1.0, 2.0])
+# orienty = np.array([  1.0,  -1.0, 0.0])
+# orientz = np.array([  1.0,  1.0,  1.0])
+# pythagorasFactorx = np.linalg.norm(orientx)
+# pythagorasFactory = np.linalg.norm(orienty)
+# pythagorasFactorz = np.linalg.norm(orientz)
+# xdir_celllength = lattice_constant*pythagorasFactorx
+# ydir_celllength = lattice_constant*pythagorasFactory
+# zdir_celllength = lattice_constant*pythagorasFactorz
+# xdir_supercelllength = xdir_celllength*Ncellx
+# ydir_supercelllength = ydir_celllength*Ncelly
+# zdir_supercelllength = zdir_celllength*Ncellz
 
 ## lattice bcc, orientation [1,0,0] [0,1,1] [0,1,1]
 Ncellx = 3
