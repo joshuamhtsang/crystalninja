7651c17cb87345ba4b20b25dec46a7b266c1fc7e
Joshua Tsang <joshua.mh.tsang@gmail.com>
2017-07-20 11:23:08 +0100

    ok, fixed the test.py script for generating bcc, i now need to code in orthogonality checks for the orientxyz vectors

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import numpy as np
 import crystalninja as cn
 
-origin = np.array([1.0,2.0,3.0])
+origin = np.array([0.0,0.0,0.0])
 
 
 ## lattice fcc ${latticeConstant} orient x 1 0 0 orient y 0 1 0 orient z 0 0 1
@@ -21,19 +21,19 @@ origin = np.array([1.0,2.0,3.0])
 #orientz = np.array([0.0, 0.0, 1.0])
 
 ## lattice fcc ${latticeConstant} orient x 1 0 0 orient y 0 1 1 orient z 0 -1 1
-Ncellx = 4
-Ncelly = 4
-Ncellz = 4
-lattice_constant = 1.0
-xdir_celllength = lattice_constant
-ydir_celllength = np.sqrt(2)*lattice_constant
-zdir_celllength = np.sqrt(2)*lattice_constant
-xdir_supercelllength = xdir_celllength*Ncellx
-ydir_supercelllength = ydir_celllength*Ncelly
-zdir_supercelllength = zdir_celllength*Ncellz
-orientx = np.array([ 1.0,  0.0, 0.0])
-orienty = np.array([ 0.0,  1.0, 1.0])
-orientz = np.array([ 0.0, -1.0, 1.0])
+#Ncellx = 4
+#Ncelly = 4
+#Ncellz = 4
+#lattice_constant = 1.0
+#xdir_celllength = lattice_constant
+#ydir_celllength = np.sqrt(2)*lattice_constant
+#zdir_celllength = np.sqrt(2)*lattice_constant
+#xdir_supercelllength = xdir_celllength*Ncellx
+#ydir_supercelllength = ydir_celllength*Ncelly
+#zdir_supercelllength = zdir_celllength*Ncellz
+#orientx = np.array([ 1.0,  0.0, 0.0])
+#orienty = np.array([ 0.0,  1.0, 1.0])
+#orientz = np.array([ 0.0, -1.0, 1.0])
 
 ## lattice fcc ${latticeConstant} orient x -1 -1 2 orient y 1 -1 0 orient z 1 1 1
 #Ncellx = 1
@@ -51,14 +51,14 @@ orientz = np.array([ 0.0, -1.0, 1.0])
 #orientz = np.array([ 1.0,  1.0, 1.0])
 
 
-## lattice bcc
+## lattice bcc, orientation [1,0,0] [0,1,1] [0,1,1]
 Ncellx = 3
 Ncelly = 3
 Ncellz = 3
 lattice_constant = 1.0
-orientx = np.array([ 1.0,  0.0, 0.0])
-orienty = np.array([ 0.0,  1.0, 1.0])
-orientz = np.array([ 0.0, -1.0, 1.0])
+orientx = np.array([ 1.0,  1.0, -2.0])
+orienty = np.array([ 1.0,  -1.0, 0.0])
+orientz = np.array([ 1.0,  1.0, 1.0])
 pythagorasFactorx = np.linalg.norm(orientx)
 pythagorasFactory = np.linalg.norm(orienty)
 pythagorasFactorz = np.linalg.norm(orientz)
